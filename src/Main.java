afb4c32e0c23480881bd89114707d45bbc57ec69
AldeaCarla <www.carlaaldea@gmail.com>
2025-03-18 08:29:42 +0200

    commit1

---
+++ src/Main.java
@@ -0,0 +1,6 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public static void main(String[] args) {
+    PasswordMaker pm = new PasswordMaker("Alice");
+    System.out.println("Generated Password: " + pm.getPassword());
+}
